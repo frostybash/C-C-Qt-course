83fc8afbdc13194c39984ef0d9cd35aac8ddee4c
frostybash <frosty_bash@mail.ru>
2019-10-29 10:05:36 +0300

    Second task on c++ course

--- Homework_2/2_dop_dop.c
+++ Homework_2/2_dop_dop.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdbool.h>
+//#include <stdbool.h>
         int main() 
         {
         double x;
         //float y;
-        long double a;
-        long double b;
+        double a=1.7976931348623158e+308;
+        double b=1.7976931348623158e+308;
         printf("was x:%d\n",x);
         //while (x>=y){
         x=a*b;
