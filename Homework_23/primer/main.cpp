d8a9cc513cca0c6343e8b80f68d9a87812c9b53e
frostybash <frosty_bash@mail.ru>
2019-12-21 13:51:29 +0300

    continue to develop

--- Homework_23/primer/main.cpp
+++ Homework_23/primer/main.cpp
@@ -4,42 +4,54 @@
 #include <QtCharts/QLineSeries>
 #include <QTextStream>
 #include <QDateTime>
+#include <QRandomGenerator>
 QT_CHARTS_USE_NAMESPACE
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qsrand(QDateTime::currentMSecsSinceEpoch() / 0.001);
-    QString path ="/home/igor/primer/";
+    QRandomGenerator prng1(100), prng2(100);
+          Q_ASSERT(prng1.generate() == prng2.generate());
+          Q_ASSERT(prng1.generate64() == prng2.generate64());
+    int x = QRandomGenerator::global()->generate();
+    int y = QRandomGenerator::global()->generate();
+    //qsrand(QDateTime::currentMSecsSinceEpoch() / 1000);
+    QString path ="/home/igor/C-C-Qt-course/Homework_23/primer";
     QString filename ="dataX.txt";
     QFile file(path+"dataX.txt");
-    if (!file.open(QIODevice::WriteOnly | QIODevice::Text))
-    QTextStream out11 (&file);
-    for (int i=0; i<1000;i++)
-    {
-
-        out11  << qrand()<< "\n";
-    }
+      if (file.open(QFile::WriteOnly | QFile::Truncate)) {
+          QTextStream out(&file);
+          for (int i=0; i<1000;i++)
+              {
+                out << QRandomGenerator::global()->generate() << "\n";
+          }
+      }
     file.close();
 
-    QString path2 ="/home/igor/primer/";
     QString filename2 ="dataY.txt";
-    QFile file2(path2+"dataY.txt");
-    if (!file2.open(QIODevice::WriteOnly | QIODevice::Text))
-    QTextStream out2 (&file2);
-    for (int j=0; j<1000;j++)
-    {
-        out2  << qrand()<< "\n";
-    }
-
+    QFile file2(path+"dataY.txt");
+      if (file2.open(QFile::WriteOnly | QFile::Truncate)) {
+          QTextStream out2(&file);
+          for (int j=0; j<1000;j++)
+              {
+                out2 << QRandomGenerator::global()->generate() << "\n";
+          }
+      }
     file2.close();
+    QFile file3("primerdataX.txt");
+         if (!file3.open(QIODevice::ReadOnly | QIODevice::Text))
 
-   /* QLineSeries *series = new QLineSeries();
-    series->append(0, 6);
-    series->append(2, 4);
-    series->append(3, 8);
-    series->append(7, 4);
-    series->append(10, 5);
+         QTextStream in1(&file3);
+         while (in1.atEnd()) {
+             QString line = in1.readLine();
+            // process_line(line);
+         }
+    QLineSeries *series = new QLineSeries();
+    series->append(x,y);
+    series->append(x,y);
+    series->append(x,y);
+    series->append(x,y);
+    series->append(x,y);
     QChart *chart = new QChart();
     chart->legend()->hide();
     chart->addSeries(series);
@@ -50,7 +62,7 @@ int main(int argc, char *argv[])
     QMainWindow window;
     window.setCentralWidget(chartView);
     window.resize(1000, 1000);
-    window.show();*/
+    window.show();
 
     return a.exec();
 }
